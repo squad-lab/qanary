831cfcb6179debbedfce99d0ad97c701a4f00294
Spandan Anupam <surelynottrue@gmail.com>
2023-10-19 14:13:43 +0000

    Update instruments.py

--- qcutils/instruments.py
+++ qcutils/instruments.py
@@ -59,19 +59,6 @@ class Lockin(Instrument):
                 self.ac = self.core.sigins[0].ac
                 self.tc = self.core.demods[0].timeconstant
                 self.order = self.core.demods[0].order
-                self.tc_sugg = 5 / self.frequency()
-
-                filter_settling = {
-                    1: 3 * self.tc(),
-                    2: 4.7 * self.tc(),
-                    3: 6.3 * self.tc(),
-                    4: 7.8 * self.tc(),
-                    5: 9.2 * self.tc(),
-                    6: 11 * self.tc(),
-                    7: 12 * self.tc(),
-                    8: 13 * self.tc(),
-                }
-                self.t_avg = filter_settling[int(self.order())]
 
                 self.autosigout = self.core.sigouts[0].autorange
                 self.autovoltin = self.core.sigins[0].autorange
@@ -132,6 +119,19 @@ class Lockin(Instrument):
             self.sinc = self.core.sync_filter
             self.tc = self.core.time_constant
             self.order = self.filter_slope
+
+    def delay(self, order, tc):
+        filter_settling = {
+                    1: 3 * tc,
+                    2: 4.7 * tc,
+                    3: 6.3 * tc,
+                    4: 7.8 * tc,
+                    5: 9.2 * tc,
+                    6: 11 * tc,
+                    7: 12 * tc,
+                    8: 13 * tc,
+                }
+        return filter_settling[int(order)]
 
     def r_val(self, demods=0):
         return abs(
